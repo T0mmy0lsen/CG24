e474b80779efd8dc9c4a88502b24e2a1f4a3f9c7
Tommy Olsen <tommy@live.dk>
2016-01-20 17:15:20 +0100

    Hello World

--- app/src/main/java/com/example/tommy/cg24/Main.java
+++ app/src/main/java/com/example/tommy/cg24/Main.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
@@ -15,5 +16,8 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello World");
     }
 }
